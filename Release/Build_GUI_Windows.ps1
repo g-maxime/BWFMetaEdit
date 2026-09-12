eb40adb2bdfa760f0fd0d0a46f3b23d651dc691d
Maxime Gervais <gervais.maxime@gmail.com>
2026-09-12 14:20:24 +0200

    Scripts 2
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- Release/Build_GUI_Windows.ps1
+++ Release/Build_GUI_Windows.ps1
@@ -44,9 +44,10 @@ if (-Not (Test-Path -Path "${release_directory}\..\Source\ThirdParty\c2pa-rs\tar
 #-----------------------------------------------------------------------
 # Build
 Push-Location -Path "${release_directory}\..\Project\CMake\GUI"
-    New-Item -Force -ItemType Directory "${arch}"
-    Push-Location -Path "${arch}"
-        cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DENABLE_C2PA=YES -DC2PA_DYNAMIC=YES $($runtime ? "-DCMAKE_RUNTIME_LIBRARY=$runtime" : "") ..
-        cmake --build .
-    Pop-Location
+    $cmake_args = @("-GNinja", "-B${arch}", "-DCMAKE_BUILD_TYPE=Release", "-DENABLE_C2PA=YES", "-DC2PA_DYNAMIC=YES")
+    if ($runtime) {
+        $cmake_args += "-DCMAKE_MSVC_RUNTIME_LIBRARY=${runtime}"
+    }
+    cmake @cmake_args .
+    cmake --build "${arch}" --parallel
 Pop-Location
